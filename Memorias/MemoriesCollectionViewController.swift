45dd81c6882966d0774b88eed0c87ca22b5035ef
Luis Conde <conderodriguez.luis@outlook.com>
2018-01-19 12:38:58 -0600

    .

--- Memorias/MemoriesCollectionViewController.swift
+++ Memorias/MemoriesCollectionViewController.swift
@@ -13,17 +13,25 @@ import Speech
 
 private let reuseIdentifier = "Cell"
 
-class MemoriesCollectionViewController: UICollectionViewController {
+class MemoriesCollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var memories: [URL] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        
+        self.loadMemories()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.addImagePressed))
 
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     
@@ -34,7 +42,7 @@ class MemoriesCollectionViewController: UICollectionViewController {
     }
     
     func checkForGrantedPermissions(){
-    
+        
         
         let photosAuth: Bool = PHPhotoLibrary.authorizationStatus() == .authorized
         let recordingAuth: Bool = AVAudioSession.sharedInstance().recordPermission() == .granted
@@ -53,17 +61,150 @@ class MemoriesCollectionViewController: UICollectionViewController {
         }
         
     }
+   
+    
+    //MARK: - Mis funciones
+    
+    func loadMemories() {
+    
+    
+        self.memories.removeAll()
+
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else {
+        
+            return
+        
+        }
+        
+        
+        for file in files {
+        
+            let fileName = file.lastPathComponent
+            
+            if fileName.hasSuffix(".thumb"){
+                
+                print(fileName)
+            
+                let noExtension = fileName.replacingOccurrences(of: ".thumb", with: "")
+                
+                if let memoryPath =  try? getDocumentsDirectory().appendingPathComponent(noExtension) {
+                    
+                    memories.append(memoryPath)
+                
+                }
+            
+            }
+            
+            
+        }
+        
+    
+    
+    }
+    
+    
+    func getDocumentsDirectory() -> URL {
+    
+    
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
+        
+        
+    }
+    
+    
+    func addImagePressed(){
+     
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet
+        vc.delegate = self
+        
+        navigationController?.present(vc, animated: true, completion: nil)
+        
+        
+    }
     
     
     
+    //MARK: - UIImagePickerControllerDelegate
     
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let theImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            self.addNewImage(image: theImage)
+            self.loadMemories()
+            
+            dismiss(animated: true)
+            
+        }else{
+            print("Algo salió mal en picker controller")
+        }
+    }
+    
+    
+    
+    func addNewImage(image: UIImage) {
+    
+        let memoryName = "memory-\(Date().timeIntervalSince1970)"
+        
+        let imageName = "\(memoryName).jpg"
+        let thumbName = "\(memoryName).thumb"
+        
+        do {
+        
+            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+            
+            if let jpegData =  UIImageJPEGRepresentation(image, 80) {
+            
+                try jpegData.write(to: imagePath, options: [.atomicWrite])
+                
+            }
+            
+            
+            if let thumbnail = resizeImage(image: image, to: 200) {
+            
+                let thumbPath = try getDocumentsDirectory().appendingPathComponent(thumbName)
+                
+                if let jpegData = UIImageJPEGRepresentation(thumbnail, 80) {
+                
+                    try jpegData.write(to: thumbPath, options: [.atomicWrite])
+                    
+                }
+                
+            }
+            
+            
+        }catch{
+        
+            print("Ha fallado la escritura en disco")
+            
+        }
+        
+    }
+    
+    
+    func resizeImage(image: UIImage, to width: CGFloat) -> UIImage? {
+    
+        let scaleFactor = width/image.size.width
+        let height = image.size.height * scaleFactor
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
     
     
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
     /*
     // MARK: - Navigation
